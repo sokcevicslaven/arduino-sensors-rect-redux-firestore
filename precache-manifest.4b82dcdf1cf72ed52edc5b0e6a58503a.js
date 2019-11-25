27c4b74e4f0022c372f4d05a992809cae14268d2
Ivan Brajković <44271953+ivbrajkovic@users.noreply.github.com>
2019-11-25 01:06:52 +0100

    Updates

---
+++ precache-manifest.4b82dcdf1cf72ed52edc5b0e6a58503a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ab6d99a8e9883527e116cdf2034b46",
+    "url": "/arduino-sensors/index.html"
+  },
+  {
+    "revision": "dcb48229c59f8f4316b7",
+    "url": "/arduino-sensors/static/css/main.e9d5f8b3.chunk.css"
+  },
+  {
+    "revision": "05d32fb1a1252ef4ba97",
+    "url": "/arduino-sensors/static/js/2.a1c28a9d.chunk.js"
+  },
+  {
+    "revision": "dcb48229c59f8f4316b7",
+    "url": "/arduino-sensors/static/js/main.bce8568a.chunk.js"
+  },
+  {
+    "revision": "c8913a206bfc5cbee988",
+    "url": "/arduino-sensors/static/js/runtime-main.bbed8622.js"
+  }
+]);
