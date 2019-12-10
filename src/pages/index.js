d3bd0ea0e9a43ee7e174a65adbbf820ee2d27ec8
Ivan Brajković <44271953+ivbrajkovic@users.noreply.github.com>
2019-12-10 23:33:35 +0100

    add useRedirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Signup from '../pages/auth/Signup';
 import Dashboard from '../pages/Dashboard';
 import Settings from '../pages/Settings';
 import Dev from '../pages/Dev';
+import History from '../pages/History';
 import About from '../pages/About';
 
-export { Login, Signup, Dashboard, Settings, Dev, About };
+export { Login, Signup, Dashboard, Settings, Dev, History, About };
